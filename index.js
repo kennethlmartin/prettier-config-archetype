7946ce454fc89244df95af168b605bea8d87e6e9
Kenneth Martin <developer@kennethmartin.me>
2021-10-10 14:31:46 -0400

    feat: remove deprecated jsxBracketSameLine

--- index.js
+++ index.js
@@ -1,53 +1,128 @@
 module.exports = {
-  // https://prettier.io/docs/en/options.html#arrow-function-parentheses
-  arrowParens: 'always',
+  /**
+   * Arrow Function Parentheses
+   *
+   * @see https://prettier.io/docs/en/options.html#arrow-function-parentheses
+   */
+  arrowParens: 'always', // default: always
 
-  // https://prettier.io/docs/en/options.html#bracket-spacing
-  bracketSpacing: true,
+  /**
+   * Bracket Line
+   *
+   * @see https://prettier.io/docs/en/options.html#bracket-line
+   */
+  bracketSameLine: false, // default: false
 
-  // https://prettier.io/docs/en/options.html#end-of-line
-  endOfLine: 'lf',
+  /**
+   * Bracket Spacing
+   *
+   * @see https://prettier.io/docs/en/options.html#bracket-spacing
+   */
+  bracketSpacing: true, // default: true
 
-  // https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
-  htmlWhitespaceSensitivity: 'css',
+  /**
+   * Embedded Language Formatting
+   *
+   * @see https://prettier.io/docs/en/options.html#embedded-language-formatting
+   */
+  embeddedLanguageFormatting: 'auto', // default: auto
 
-  // https://prettier.io/docs/en/options.html#insert-pragma
-  insertPragma: false,
+  /**
+   * End of Line
+   *
+   * @see https://prettier.io/docs/en/options.html#end-of-line
+   */
+  endOfLine: 'lf', // default: lf
 
-  // https://prettier.io/docs/en/options.html#jsx-brackets
-  jsxBracketSameLine: false,
+  /**
+   * HTML Whitespace Sensitivity
+   *
+   * @see https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
+   */
+  htmlWhitespaceSensitivity: 'css', // default: css
 
-  // https://prettier.io/docs/en/options.html#jsx-quotes
-  jsxSingleQuote: false,
+  /**
+   * nsert Pragma
+   *
+   * @see https://prettier.io/docs/en/options.html#insert-pragma
+   */
+  insertPragma: false, // default: false
 
-  // https://prettier.io/docs/en/options.html#print-width
-  printWidth: 80,
+  /**
+   * JSX Quotes
+   *
+   * @see https://prettier.io/docs/en/options.html#jsx-quotes
+   */
+  jsxSingleQuote: false, // default: false
 
-  // https://prettier.io/docs/en/options.html#prose-wrap
-  proseWrap: 'preserve',
+  /**
+   * Print Width
+   *
+   * @see https://prettier.io/docs/en/options.html#print-width
+   */
+  printWidth: 80, // default: 80
 
-  // https://prettier.io/docs/en/options.html#quote-props
-  quoteProps: 'consistent',
+  /**
+   * Prose Wrap
+   *
+   * @see https://prettier.io/docs/en/options.html#prose-wrap
+   */
+  proseWrap: 'preserve', // default: preserve
 
-  // https://prettier.io/docs/en/options.html#range
-  rangeEnd: Infinity,
-  rangeStart: 0,
+  /**
+   * Quote Props
+   *
+   * @see https://prettier.io/docs/en/options.html#quote-props
+   */
+  quoteProps: 'consistent', // default: 'as-needed'
 
-  // https://prettier.io/docs/en/options.html#require-pragma
-  requirePragma: false,
+  /**
+   * Range
+   *
+   * @see https://prettier.io/docs/en/options.html#range
+   */
+  rangeEnd: Infinity, // default: Infinity
+  rangeStart: 0, // default: 0
 
-  // https://prettier.io/docs/en/options.html#semicolons
-  semi: true,
+  /**
+   * Require Pragma
+   *
+   * @see https://prettier.io/docs/en/options.html#require-pragma
+   */
+  requirePragma: false, // default: false
 
-  // https://prettier.io/docs/en/options.html#quotes
-  singleQuote: true,
+  /**
+   * Semicolons
+   *
+   * @see https://prettier.io/docs/en/options.html#semicolons
+   */
+  semi: true, // default: true
 
-  // https://prettier.io/docs/en/options.html#tab-width
-  tabWidth: 2,
+  /**
+   * Quotes
+   *
+   * @see https://prettier.io/docs/en/options.html#quotes
+   */
+  singleQuote: true, // default: false
 
-  // https://prettier.io/docs/en/options.html#trailing-commas
-  trailingComma: 'all',
+  /**
+   * Tab Width
+   *
+   * @see https://prettier.io/docs/en/options.html#tab-width
+   */
+  tabWidth: 2, // default: 2
 
-  // https://prettier.io/docs/en/options.html#tabs
-  useTabs: false,
+  /**
+   * Trailing Commas
+   *
+   * @see https://prettier.io/docs/en/options.html#trailing-commas
+   */
+  trailingComma: 'all', // default: es5
+
+  /**
+   * Tabs
+   *
+   * @see https://prettier.io/docs/en/options.html#tabs
+   */
+  useTabs: false, // default: false
 };
